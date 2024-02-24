6facaf59c1c0b45aa3a9b4b11824959e233a3ade
Subhradeep <beradeep35@gmail.com>
2024-02-24 21:46:00 +0530

    added postgres

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const db = require('./db')
+const pool = require('./db')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+pool.on('connect', () => {
+  console.log('pool connect success!');
 });
