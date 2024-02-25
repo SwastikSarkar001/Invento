0e8b7bfe17397262df520e8183d26ab1dac90498
Subhradeep <beradeep35@gmail.com>
2024-02-25 11:01:21 +0530

    fix gen-ai

--- server/index.js
+++ server/index.js
@@ -178,7 +178,7 @@ app.get('/api/gen-ai', async (req, res) => {
     try {
         const genAI = new GoogleGenerativeAI(process.env.API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-pro"});
-        const prompt = "Write a story about a magic backpack."
+        const { prompt } = req.body;
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
